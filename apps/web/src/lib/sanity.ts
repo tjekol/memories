7358829654be49d6e1c6e20805184016b0c0f012
Thea Kolnes <thea.jenny02@gmail.com>
2023-10-10 01:37:59 +0200

    import images from sanity

---
+++ apps/web/src/lib/sanity.ts
@@ -0,0 +1,12 @@
+import { createClient } from 'next-sanity';
+
+const projectId = '97u2fmo5';
+const dataset = 'production';
+const apiVersion = '2021-10-21';
+
+export const sanityClient = createClient({
+  projectId,
+  dataset,
+  apiVersion,
+  useCdn: true,
+});
