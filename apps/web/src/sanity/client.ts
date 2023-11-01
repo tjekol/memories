c5e94f80d8099e4410b352f3e893bd9384d8468b
Thea Kolnes <thea.jenny02@gmail.com>
2023-11-01 22:18:06 +0100

    upd next, upd fetch

--- apps/web/src/sanity/client.ts
+++ apps/web/src/sanity/client.ts
@@ -2,7 +2,7 @@ import { createClient } from 'next-sanity';
 
 const projectId = '97u2fmo5';
 const dataset = 'production';
-const apiVersion = '2023-10-09';
+const apiVersion = 'v2023-10-09';
 
 export const client = createClient({
   projectId,
